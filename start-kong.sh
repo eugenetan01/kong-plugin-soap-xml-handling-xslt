be809b0e4e06ebe8435c68ab73d103cb9bf00126
eugenetan01 <eugenetanweihong@gmail.com>
2024-10-23 18:01:10 +0800

    init commit

--- start-kong.sh
+++ start-kong.sh
@@ -1,6 +1,24 @@
 # Delete the Kong Gateway container
 docker rm -f kong-gateway-soap-xml-handling
 
+docker network create kong-net
+
+docker run -d --name kong-database-soap-xml-handling \
+  --network=kong-net \
+  -e POSTGRES_USER=kong \
+  -e POSTGRES_PASSWORD=kongpass \
+  -e POSTGRES_DB=kong \
+  -p 5432:5432 \
+  postgres:14
+
+docker run --rm --network=kong-net \
+    -e "KONG_DATABASE=postgres" \
+    -e "KONG_PG_HOST=kong-database-soap-xml-handling" \
+    -e "KONG_PG_USER=kong" \
+    -e "KONG_PG_PASSWORD=kongpass" \
+    -e "KONG_PASSWORD=test" \
+    kong/kong-gateway:3.8.0.0 kong migrations bootstrap
+
 export ARCHITECTURE=arm64
 
 # Start Kong Gateway
@@ -20,20 +38,20 @@ docker run -d --name kong-gateway-soap-xml-handling \
 -e "KONG_ADMIN_ACCESS_LOG=/dev/stdout" \
 -e "KONG_PROXY_ERROR_LOG=/dev/stderr" \
 -e "KONG_ADMIN_ERROR_LOG=/dev/stderr" \
--e "KONG_PROXY_LISTEN=0.0.0.0:7000, 0.0.0.0:7443 ssl http2" \
--e "KONG_ADMIN_LISTEN=0.0.0.0:7001, 0.0.0.0:7444 ssl http2" \
--e "KONG_ADMIN_GUI_LISTEN=0.0.0.0:7002, 0.0.0.0:7445 ssl" \
--e "KONG_ADMIN_GUI_URL=http://localhost:7002" \
+-e "KONG_PROXY_LISTEN=0.0.0.0:1000, 0.0.0.0:1443 ssl http2" \
+-e "KONG_ADMIN_LISTEN=0.0.0.0:1001, 0.0.0.0:1444 ssl http2" \
+-e "KONG_ADMIN_GUI_LISTEN=0.0.0.0:1002, 0.0.0.0:1445 ssl" \
+-e "KONG_ADMIN_GUI_URL=http://localhost:1002" \
 -e "KONG_PLUGINS=bundled,soap-xml-request-handling,soap-xml-response-handling" \
 -e "KONG_LOG_LEVEL=notice" \
 -e "LD_LIBRARY_PATH=/usr/local/lib/kongsaxon" \
 -e "KONG_NGINX_WORKER_PROCESSES=1" \
 -e KONG_LICENSE_DATA \
--p 7000:7000 \
--p 7443:7443 \
--p 7001:7001 \
--p 7002:7002 \
--p 7444:7444 \
+-p 1000:1000 \
+-p 1443:1443 \
+-p 1001:1001 \
+-p 1002:1002 \
+-p 1444:1444 \
 --platform linux/$ARCHITECTURE \
 kong/kong-gateway:3.8.0.0
 
